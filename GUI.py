08ec3ad3ada88d7b1f398cec86ae52158071f225
Odysseus Valdez <odysseusdrums@unm.edu>
2026-05-02 21:26:50 -0600

    GUI Initial version finished, need to work out a couple bugs

---
+++ GUI.py
@@ -0,0 +1,18 @@
+import PyQt5
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QDialog, QVBoxLayout, QHBoxLayout, QPushButton, QLineEdit, QLabel, QComboBox, QCheckBox, QSpinBox, QDoubleSpinBox, QMessageBox
+from PyQt5.QtGui import QIntValidator, QDoubleValidator
+
+
+class GSM_GUI:
+    
+    def __init__(self):
+        
+
+
+
+
+
+
+if __name__ == "__main__":
+    gui = GSM_GUI()
